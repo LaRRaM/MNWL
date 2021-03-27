e257be6fa31ed31df255c12affe9e3c4ff520017
LaRRaM <LaRRaM@in.c>
2021-03-27 22:24:04 +0300

    v0.3

--- src/mnwl/plugin/Main.java
+++ src/mnwl/plugin/Main.java
@@ -12,7 +12,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
+//some comment
 public class Main extends JavaPlugin implements Listener{
 	@Override
 	public void onEnable() {
